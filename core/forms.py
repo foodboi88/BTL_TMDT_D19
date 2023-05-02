7a60190a47a41c8b9363b005300c78ff4754ab3b
Nguyễn Công Hậu <82644341+Guzamo1000@users.noreply.github.com>
2023-05-02 10:21:56 +0700

    add comment

--- core/forms.py
+++ core/forms.py
@@ -2,14 +2,15 @@ from django import forms
 from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
 
-
+# Định nghĩa các lựa chọn cho phương thức thanh toán
 PAYMENT_CHOICES = (
     ('S', 'Stripe'),
     ('P', 'PayPal')
 )
 
-
+# Biểu mẫu để nhập thông tin địa chỉ và phương thức thanh toán
 class CheckoutForm(forms.Form):
+    # Thông tin địa chỉ giao hàng
     shipping_address = forms.CharField(required=False)
     shipping_address2 = forms.CharField(required=False)
     shipping_country = CountryField(blank_label='(select country)').formfield(
@@ -19,6 +20,7 @@ class CheckoutForm(forms.Form):
         }))
     shipping_zip = forms.CharField(required=False)
 
+    # Thông tin địa chỉ thanh toán
     billing_address = forms.CharField(required=False)
     billing_address2 = forms.CharField(required=False)
     billing_country = CountryField(blank_label='(select country)').formfield(
@@ -28,16 +30,18 @@ class CheckoutForm(forms.Form):
         }))
     billing_zip = forms.CharField(required=False)
 
+    # Các trường checkbox để lưu thông tin thanh toán và giao hàng mặc định
     same_billing_address = forms.BooleanField(required=False)
     set_default_shipping = forms.BooleanField(required=False)
     use_default_shipping = forms.BooleanField(required=False)
     set_default_billing = forms.BooleanField(required=False)
     use_default_billing = forms.BooleanField(required=False)
 
+    # Tùy chọn phương thức thanh toán
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
 
-
+# Biểu mẫu để nhập mã giảm giá
 class CouponForm(forms.Form):
     code = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
@@ -46,7 +50,7 @@ class CouponForm(forms.Form):
         'aria-describedby': 'basic-addon2'
     }))
 
-
+# Biểu mẫu để yêu cầu hoàn tiền
 class RefundForm(forms.Form):
     ref_code = forms.CharField()
     message = forms.CharField(widget=forms.Textarea(attrs={
@@ -54,7 +58,7 @@ class RefundForm(forms.Form):
     }))
     email = forms.EmailField()
 
-
+# Biểu mẫu để thanh toán
 class PaymentForm(forms.Form):
     stripeToken = forms.CharField(required=False)
     save = forms.BooleanField(required=False)
