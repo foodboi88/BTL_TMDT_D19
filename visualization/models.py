bd976bc9313583fe74d4c794baa1944c5207d785
Nguyễn Công Hậu <82644341+Guzamo1000@users.noreply.github.com>
2023-05-02 10:16:24 +0700

    Viet comment cho toàn bộ hàm

--- visualization/models.py
+++ visualization/models.py
@@ -325,47 +325,57 @@ def funtion8(year="2022"):
     end_date = year+"-12-31"
     query = f"select core_order.id, ordered_date,ordered, amount from core_order, core_payment where ordered_date>='{start_date}' and ordered_date<='{end_date}' and core_order.payment_id=core_payment.id"
     df = pd.read_sql(query, conn)
+    
     # Chuyển đổi cột "ordered_date" sang định dạng datetime
     df['ordered_date'] = pd.to_datetime(df['ordered_date'])
     df['month'] = df['ordered_date'].dt.month
+    
     # Nhóm dữ liệu theo "year" và "ordered" và tính tổng số đơn hàng và tổng số tiền
     result = df.groupby(['month', 'ordered']).agg(
         {'ordered': 'count', 'amount': 'sum'})
     result = result.rename(columns={'ordered': 'count'})
+    
     months = result.index.get_level_values('month').unique().tolist()
-    revenues_ordered_0 = result[result.index.get_level_values(
-        'ordered') == 0]['amount'].groupby('month').sum().tolist()
-    revenues_ordered_1 = result[result.index.get_level_values(
-        'ordered') == 1]['amount'].groupby('month').sum().tolist()
-    my_dict_0 = {'1': 0, '2': 0, '3': 0,  '4': 0, '5': 0, '6': 0,
-                 '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0}
-    my_dict_1 = {'1': 0, '2': 0, '3': 0,  '4': 0, '5': 0, '6': 0,
-                 '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0}
+    revenues_ordered_0 = result[result.index.get_level_values('ordered') == 0]['amount'].groupby('month').sum().tolist()
+    revenues_ordered_1 = result[result.index.get_level_values('ordered') == 1]['amount'].groupby('month').sum().tolist()
+    
+    my_dict_0 = {'1': 0, '2': 0, '3': 0,  '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0}
+    my_dict_1 = {'1': 0, '2': 0, '3': 0,  '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0}
 
+    # Chuyển đổi list sang dictionary
     for i in range(len(months)):
         months[i] = str(months[i])
         my_dict_0[str(months[i])] = revenues_ordered_0[i]
         my_dict_1[str(months[i])] = revenues_ordered_1[i]
+    
     X = my_dict_0.keys()
     order_date = my_dict_0.values()
     ordered = my_dict_1.values()
     X_axis = np.arange(len(X))
+    
+    # Vẽ biểu đồ cột
     plt.bar(X_axis - 0.2, order_date, 0.4, label='Đơn hàng')
     plt.bar(X_axis + 0.2, ordered, 0.4, label='Hóa đơn')
     plt.xticks(X_axis, X)
     plt.xlabel("Tháng")
     plt.ylabel("Doanh thu")
-    plt.title("BẢng thống kê")
+    plt.title("Bảng thống kê")
     plt.legend()
-    filename = save_image()
-
+    
+    filename = save_image()  # Lưu biểu đồ thành hình ảnh
+    
     return filename, my_dict_0, my_dict_1
+
 
 
 def funtion9(year="2022", quantity=5):
     import json
+    
+    # Tạo chuỗi ngày bắt đầu và kết thúc dựa trên năm đầu vào
     start_date = year+"-01-01"
     end_date = year+"-12-31"
+    
+    # Tạo câu truy vấn SQL để lấy thông tin đơn hàng và số lượng sản phẩm đã bán được
     query = f"""SELECT core_item.title, SUM(core_orderitem.quantity) AS total_quantity
             FROM core_order_items
             INNER JOIN core_orderitem ON core_orderitem.id = core_order_items.orderitem_id
@@ -377,31 +387,49 @@ def funtion9(year="2022", quantity=5):
             ORDER BY total_quantity DESC
             LIMIT {quantity};
         """
+    
+    # Đọc dữ liệu từ câu truy vấn SQL và chuyển đổi thành dataframe
     df = pd.read_sql(query, conn)
+    
+    # Đặt lại chỉ số của dataframe
     df = df.reset_index(drop=True)
+    
+    # Tạo 2 list lưu trữ tiêu đề sản phẩm và số lượng sản phẩm đã bán được
     title = df["title"].to_list()
     total_quantity = df["total_quantity"].to_list()
+    
+    # Tạo dictionary lưu trữ thông tin của sản phẩm
     my_dict = {key: value for key, value in zip(title, total_quantity)}
+    
+    # Tạo 2 list chứa thông tin của các sản phẩm để vẽ biểu đồ pie chart
     title = my_dict.keys()
     total_quantity = my_dict.values()
+    
+    # Tạo list chứa tỷ lệ phần trăm của từng sản phẩm
     explode = [0.1]*len(title)
-
+    
+    # Vẽ biểu đồ pie chart dựa trên thông tin của các sản phẩm
     plt.pie(total_quantity, explode=explode, labels=title,
             autopct='%1.1f%%', startangle=0,
             wedgeprops={"edgecolor": "white",
                         'linewidth': 5,
                         'antialiased': True})
     plt.axis('equal')
-
+    
+    # Lưu biểu đồ pie chart dưới dạng file ảnh và trả về tên file ảnh cùng với dictionary chứa thông tin của các sản phẩm
     filename = save_image()
-
     return filename, my_dict
+
 
 
 def funtion10(year="2022", quantity=5):
     import json
-    start_date = year+"-01-01"
-    end_date = year+"-12-31"
+
+    # Tạo chuỗi ngày bắt đầu và kết thúc năm dựa vào năm được truyền vào
+    start_date = year + "-01-01"
+    end_date = year + "-12-31"
+
+    # Truy vấn CSDL để lấy thông tin các sản phẩm có số lượng bán nhiều nhất
     query = f"""SELECT core_item.label, SUM(core_orderitem.quantity) AS total_quantity
             FROM core_order_items
             INNER JOIN core_orderitem ON core_orderitem.id = core_order_items.orderitem_id
@@ -414,25 +442,46 @@ def funtion10(year="2022", quantity=5):
             LIMIT {quantity};
         """
     df = pd.read_sql(query, conn)
+
+    # Chuyển đổi dataframe thành 2 danh sách riêng biệt chứa tên sản phẩm và tổng số lượng
     df = df.reset_index(drop=True)
     title = df["label"].to_list()
     total_quantity = df["total_quantity"].to_list()
+
+    # Tạo dictionary để lưu trữ thông tin sản phẩm và tổng số lượng tương ứng
     my_dict = {key: value for key, value in zip(title, total_quantity)}
+
+    # Chuyển đổi lại title và total_quantity từ dictionary để sử dụng cho việc vẽ biểu đồ pie chart
     title = my_dict.keys()
     total_quantity = my_dict.values()
+
+    # Tạo list explode chứa giá trị 0.1 lặp lại số lần bằng độ dài của danh sách sản phẩm
     explode = [0.1]*len(title)
 
+    # Vẽ biểu đồ pie chart dựa trên danh sách sản phẩm và tổng số lượng
     plt.pie(total_quantity, explode=explode, labels=title,
             autopct='%1.1f%%', startangle=0,
             wedgeprops={"edgecolor": "white",
                         'linewidth': 5,
                         'antialiased': True})
     plt.axis('equal')
+
+    # Lưu biểu đồ vào file ảnh và trả về tên file ảnh cùng với dictionary chứa thông tin sản phẩm và tổng số lượng tương ứng
     filename = save_image()
     return filename, my_dict
 
 
+
 def funtion11(year="2022", quantity=20):
+    """
+    Lấy thông tin các khách hàng có số lần đặt hàng nhiều nhất trong năm được chỉ định.
+    Tạo Wordcloud với từng khách hàng và số lần đặt hàng tương ứng.
+    
+    :param year: str, năm cần tìm kiếm thông tin khách hàng đặt hàng nhiều nhất.
+    :param quantity: int, số lượng khách hàng xuất hiện nhiều nhất muốn lấy thông tin.
+    
+    :return: tuple, chứa tên file hình ảnh và từ điển số lần đặt hàng tương ứng của từng khách hàng.
+    """
     start_date = year+"-01-01"
     end_date = year+"-12-31"
     query = f"""
@@ -441,15 +490,18 @@ def funtion11(year="2022", quantity=20):
         where ordered_date BETWEEN '{start_date}' and '{end_date}' and is_staff='0' and is_active='1' and ordered='1'
         """
     df = pd.read_sql(query, conn)
+    
     # Đếm số lần xuất hiện của mỗi giá trị trong cột user_id
     top_users = df['user_id'].value_counts().head(quantity).index
     user_counts = df['user_id'].value_counts().head(quantity)
+    
     # Lấy thông tin của 5 khách hàng xuất hiện nhiều nhất trong bảng
     result = df.loc[df['user_id'].isin(top_users), [
         'user_id', 'username', 'first_name', 'last_name']].drop_duplicates()
     result = result.reset_index(drop=True)
     dict_data = result.to_dict(orient='records')
     userid, counts, username = [], [], []
+    
     # Thêm số lần xuất hiện của từng user vào dict_data
     for i, user in enumerate(dict_data):
         user_id = user['user_id']
@@ -457,47 +509,69 @@ def funtion11(year="2022", quantity=20):
         counts.append(user_counts[user_id])
         username.append(user['username'])
     my_dict = {key: value for key, value in zip(username, counts)}
+    
     from wordcloud import WordCloud
     wc = WordCloud(margin=5, width=900, height=600, background_color='white')
     wc.generate_from_frequencies(my_dict)
+    
     plt.figure(figsize=(10, 6))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
     filename = save_image()
+    
     return filename, my_dict
 
 
+
+# Hàm funtion12 lấy dữ liệu từ các bảng core_payment, auth_user và core_order trong database
+# và tạo ra một wordcloud dựa trên tổng số tiền thanh toán của 5 khách hàng có tổng số tiền lớn nhất trong năm nào đó.
+
 def funtion12(year="2022", quantity=10):
+    # Đặt ngày bắt đầu và ngày kết thúc trong năm.
     start_date = year+"-01-01"
     end_date = year+"-12-31"
+    
+    # Tạo câu truy vấn SQL để lấy các thông tin cần thiết từ database.
     query = f"""SELECT core_payment.user_id,core_payment.amount , auth_user.username, auth_user.first_name, auth_user.last_name, core_order.ordered_date  FROM 'core_payment' 
             inner join auth_user on auth_user.id=core_payment.user_id
             inner join core_order on core_order.payment_id=core_payment.id
             where core_order.ordered_date between '{start_date}' and '{end_date}' and ordered='1';"""
+    
+    # Lấy dữ liệu từ database và đưa vào pandas dataframe.
     df = pd.read_sql(query, conn)
-    # Nhóm các hàng cùng user_id và tính tổng amount
+    
+    # Nhóm các hàng theo user_id và tính tổng số tiền thanh toán.
     grouped = df.groupby('user_id').agg({'amount': 'sum'}).reset_index()
 
-    # Tìm ra 5 khách hàng có tổng amount lớn nhất
+    # Lấy ra 5 khách hàng có tổng số tiền thanh toán lớn nhất.
     top_5 = grouped.nlargest(quantity, 'amount')
 
-    # Lấy ra thông tin user_id, username, first_name, last_name của các khách hàng đó
-    result = df.loc[df['user_id'].isin(top_5['user_id'])][[
-        'user_id', 'username', 'first_name', 'last_name']].drop_duplicates()
+    # Lấy thông tin user_id, username, first_name, last_name của các khách hàng đó.
+    result = df.loc[df['user_id'].isin(top_5['user_id'])][[        'user_id', 'username', 'first_name', 'last_name']].drop_duplicates()
+    
+    # Ghép các thông tin của khách hàng với tổng số tiền thanh toán của họ, sắp xếp giảm dần theo số tiền.
     result = result.merge(top_5, on='user_id').sort_values(
         by='amount', ascending=False)
+    
+    # Reset lại index của dataframe.
     result = result.reset_index(drop=True)
 
+    # Tạo dictionary với key là tên của khách hàng, value là tổng số tiền thanh toán của họ.
     data_result = result[['username', 'amount']]
     username = list(data_result['username'])
     amount = list(data_result['amount'])
     my_dict = {key: value for key, value in zip(username, amount)}
 
+    # Tạo wordcloud dựa trên dictionary vừa tạo.
     from wordcloud import WordCloud
     wc = WordCloud(margin=5, width=900, height=600, background_color='white')
     wc.generate_from_frequencies(my_dict)
+    
+    # Vẽ wordcloud lên màn hình.
     plt.figure(figsize=(10, 6))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
+    
+    # Lưu wordcloud thành file ảnh và trả về tên file cùng với dictionary.
     filename = save_image()
     return filename, my_dict
